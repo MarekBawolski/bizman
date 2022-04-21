35b9efe66c861f43a72b20134eaee8a7cc8182e3
root <root@server966566.nazwa.pl>
2022-04-21 19:31:56 +0200

    test pull requestow

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,7 @@
 
         <style>
             body {
-                font-family: 'Nunito', sans-serif;
+                font-family: 'Nunito', Helvetica, sans-serif;
             }
         </style>
 

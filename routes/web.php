9f77854bd4f6f9761564eb347abba3ca418bde05
MarekBawolski <marbaw@st.amu.edu.pl>
2022-04-24 11:25:34 +0200

    example controller with route and custom view

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ExperimentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,5 +21,8 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::get('experiment', [ExperimentController::class, 'index'])
+                ->name('experiment');
 
 require __DIR__.'/auth.php';

0d5d75ff5aed39edf052dba86fe255352c690447
wawrzon007 <222michal100@wp.pl>
2022-05-25 14:41:59 +0200

    list of clients\

--- resources/views/clients.blade.php
+++ resources/views/clients.blade.php
@@ -4,15 +4,37 @@
       {{ __('Baza klientów') }}
     </h2>
   </x-slot>
-
+  
   @isset($clients)
-    @foreach ($clients as $client)
-      <a href="{{ '/clients/' . $client->id . '/view' }}">{{ $client->first_name }} {{ $client->last_name }}</a><br>
-    @endforeach
-  @endisset ($clients)
-
-
-  <div class="flex justify-center">
-    {!! $clients->links() !!}
-  </div>
+  <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <div class="flex flex-col">
+                        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                            <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                                <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                                    <table class="min-w-full divide-y divide-gray-200">
+                                        <tbody class="bg-white divide-y divide-gray-200">
+                                        @foreach ($clients as $client)
+                                        <tr>
+                                            <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                                {{ $client->first_name }} {{ $client->last_name }}
+                                            </td>
+                                        </tr>
+                                        @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <div class="mt-4">
+                                {{ $clients->links() }}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  @endisset
 </x-app-layout>

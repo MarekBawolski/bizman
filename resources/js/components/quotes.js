366c1223e842d4d9e473babbd57ad2ff8136680e
wawrzon007 <222michal100@wp.pl>
2022-10-31 14:02:55 +0100

    adding and removing quote

--- resources/js/components/quotes.js
+++ resources/js/components/quotes.js
@@ -1,5 +1,8 @@
 document.querySelectorAll('.add-to-quote').forEach(function (element){
-    element.addEventListener('click', function (e){
+    element.addEventListener('click', function (){quote();},{once: true});
+    
+    
+    function quote(){
         console.log(element.dataset.element_id);
         console.log(element.dataset.element_title);
         console.log(element.dataset.element_content);
@@ -10,33 +13,40 @@ document.querySelectorAll('.add-to-quote').forEach(function (element){
 
     var div = document.getElementById("item"+id);
     var divClone = div.cloneNode(true);
+    divClone.id = "item"+id+"m";
     //var divTitle = document.getElementById("item"+id+"_title");
     var divT = divClone.children[1].children[0]
     
     var divCloneTitle = document.createElement("input");
-    divCloneTitle.setAttribute("id","item"+id+"_title");
+    divCloneTitle.setAttribute("id","item"+id+"_titlem");
     divCloneTitle.setAttribute("class","mb-2 font-semibold")
     divCloneTitle.value = title
+    divCloneTitle.style.width = "100%";
 
     //var divContent = document.getElementById("item"+id+"_content");
     var divC = divClone.children[1].children[1]
     var divCloneContent = document.createElement("textarea");
-    divCloneContent.setAttribute("id","item"+id+"_content")
+    divCloneContent.setAttribute("id","item"+id+"_contentm")
     divCloneContent.setAttribute("class","text-sm")
     divCloneContent.value = content
+    divCloneContent.style.width = "100%";
 
     var divButton = document.getElementById("item"+id+"_button");
+    divButton.setAttribute('class','cursor-pointer pointer-events-none btn-primary');
+    //divButton,removeEventListener('click','function(e)')
     var divB = divClone.children[0].children[0]
     var divCloneButton = divButton.cloneNode(true);
-    divButton.setAttribute('class','cursor-pointer pointer-events-none btn-primary');
-    divButton.removeAttribute('onclick')
+    divCloneButton.setAttribute("id","item"+id+"_buttonm")
     divCloneButton.setAttribute('class','cursor-pointer btn-secondary')
     divCloneButton.innerHTML = "-"
-    //divButton.setAttribute('onclick','abc('+id+')')
-    //divButton.classList.add('pointer-events-none')
-    //alert(divButton.onclick)
-      //alert(divTitle)
-      //alert(divCloneTitle)
+
+    
+    divCloneButton.addEventListener('click',function (e){
+      document.getElementById("item"+id+"m").remove()
+      document.getElementById("item"+id).style.background = "rgb(243 244 246)"
+      document.getElementById("item"+id+"_button").setAttribute('class','cursor-pointer btn-primary');
+      document.getElementById("item"+id+"_button").addEventListener('click', function (){quote();},{once: true});
+    })
 
     
     div.style.background="gray"
@@ -46,6 +56,7 @@ document.querySelectorAll('.add-to-quote').forEach(function (element){
     document.querySelectorAll('.here-add-quote').forEach(function (ele){
       ele.appendChild(divClone);
       })
-    },{once: true})
-})
+    }
+  }
+)
 
